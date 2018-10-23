04027a56f502eaf2ad73a55e62087080ea9e9d9f
dinmaker <37370379+dinmaker@users.noreply.github.com>
2018-10-23 17:34:11 +0100

    Importing and Altering code

---
+++ GameTools2_Assignment1/Assets/Character.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Character : MonoBehaviour
+{
+
+    private Animator m_animator;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        // Initialize Animator
+        m_animator = GetComponent<Animator>();
+    }
+
+    public void Move(float turn, float forward, bool jump)
+    {
+        m_animator.SetFloat("Turn", turn);
+        m_animator.SetFloat("Forward", forward);
+
+
+        if (jump)
+        {
+            m_animator.SetTrigger("Jump");
+        }
+
+    }
+}
