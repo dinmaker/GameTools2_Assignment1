e0697f673bee29be3c8f2ba4d624957e56337190
dinmaker <37370379+dinmaker@users.noreply.github.com>
2018-12-18 20:49:12 +0000

    adding frothing blood, tidying scripts

---
+++ GameTools2_Assignment1/Assets/Scripts/blood.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blood : MonoBehaviour, iPoolable
+{
+
+    [SerializeField] float sideVelocity;
+    [SerializeField] float upVelocity;
+
+    public void OnObjectPolled()
+    {
+        Vector3 velocity = new Vector3();
+        velocity.x = Random.Range(-sideVelocity, sideVelocity);
+        velocity.y = Random.Range(0, upVelocity);
+        velocity.z = Random.Range(-sideVelocity, sideVelocity);
+
+        GetComponent<Rigidbody>().velocity = velocity;
+    }
+}
---
+++ GameTools2_Assignment1/Assets/Scripts/bloosSpawn.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bloosSpawn : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    public static bloosSpawn Instance;
+
+    [SerializeField] List<Pool> pools;
+
+    Dictionary<string, Queue<GameObject>> poolsDictionary;
+
+    public void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            if (Instance != this)
+            {
+                Destroy(this);
+            }
+        }
+    }
+
+    private void Start()
+    {
+        poolsDictionary = new Dictionary<string, Queue<GameObject>>();
+        foreach (Pool pool in pools)
+        {
+            //Create queue
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            //Instantiate all elements
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject go = Instantiate(pool.prefab);
+                go.SetActive(false);
+                objectPool.Enqueue(go);
+            }
+
+            //Add to dictionary
+            poolsDictionary.Add(pool.tag, objectPool);
+        }
+    }
+    public GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation)
+    {
+        if (!poolsDictionary.ContainsKey(tag))
+        {
+            return null;
+        }
+        GameObject go = poolsDictionary[tag].Dequeue();
+        go.SetActive(true);
+        go.transform.position = position;
+        go.transform.rotation = rotation;
+        poolsDictionary[tag].Enqueue(go);
+        iPoolable objectToPool = go.GetComponent<iPoolable>();
+        objectToPool.OnObjectPolled();
+        return go;
+    }
+}
